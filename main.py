e11a2749e8aa284c976ddb9115cddea177a87dec
gmaneschy <145074809+gmaneschy@users.noreply.github.com>
2025-09-12 10:40:56 -0300

    backlog

---
+++ main.py
@@ -0,0 +1,19 @@
+import sentry_sdk
+from flask import Flask, render_template
+
+sentry_sdk.init(
+    dsn="https://2202721634f7017e585a58b72c56f7c7@o4509990068027392.ingest.us.sentry.io/4510002564825088",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+if __name__ == '__main__':
+    app.run()
