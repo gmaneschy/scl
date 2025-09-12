527227ed5169e73e9ec6945942f937445969e355
gmaneschy <145074809+gmaneschy@users.noreply.github.com>
2025-09-12 16:22:16 -0300

    backlog

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ sentry_sdk.init(
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
